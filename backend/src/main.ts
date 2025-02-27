e0b6a7a10266ec44d368d43c9948f710a03998a9
bryanguillen <bryan.guillen217@gmail.com>
2025-02-27 07:39:10 -0500

    Add support for adding cors origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors(configService.get('cors'));
+
   await app.listen(3000);
 }
 bootstrap();
