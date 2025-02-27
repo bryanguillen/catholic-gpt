668fb4c6f344baf48d71383aee5dd31a483970dc
bryanguillen <bryan.guillen217@gmail.com>
2025-02-27 08:55:07 -0500

    Initial commit

---
+++ frontend/src/components/PageLoader/PageLoader.tsx
@@ -0,0 +1,7 @@
+export const PageLoader = () => {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="h-10 w-10 animate-spin rounded-full border-t-4 border-b-4 border-gray-900 dark:border-white"></div>
+    </div>
+  );
+};
