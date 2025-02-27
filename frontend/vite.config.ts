0ae4a0b118b7f76cf6dae5792837302fe8673d9f
bryanguillen <bryan.guillen217@gmail.com>
2025-02-27 09:28:46 -0500

    Add setup files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -15,5 +16,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts', 'vitest-localstorage-mock'],
   },
 });
