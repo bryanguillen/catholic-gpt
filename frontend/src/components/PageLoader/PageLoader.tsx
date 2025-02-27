c7bc3ac25d1872e5a21faa31e20350138cbdbedb
bryanguillen <bryan.guillen217@gmail.com>
2025-02-27 08:59:17 -0500

    Add support for passing in div props for more effective reuse

--- frontend/src/components/PageLoader/PageLoader.tsx
+++ frontend/src/components/PageLoader/PageLoader.tsx
@@ -1,6 +1,17 @@
-export const PageLoader = () => {
+import { cn } from '@/lib/utils';
+
+export const PageLoader = ({
+  className,
+  ...props
+}: React.ComponentProps<'div'>) => {
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div
+      className={cn(
+        'flex h-screen w-screen items-center justify-center',
+        className
+      )}
+      {...props}
+    >
       <div className="h-10 w-10 animate-spin rounded-full border-t-4 border-b-4 border-gray-900 dark:border-white"></div>
     </div>
   );
