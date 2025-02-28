4b5c5a352f2903782d194102f555bc9b2d9001b6
bryanguillen <bryan.guillen217@gmail.com>
2025-02-28 09:46:44 -0500

    Add support for streaming the response

--- frontend/src/pages/home/components/AssistantMessage.tsx
+++ frontend/src/pages/home/components/AssistantMessage.tsx
@@ -1,5 +1,40 @@
+import { useEffect, useState } from 'react';
 import { LoadingDots } from '@/components/ui/loading';
 
-export function AssistantMessage() {
-  return <LoadingDots />;
+interface AssistantMessageProps {
+  conversationId: string;
+}
+
+export function AssistantMessage({ conversationId }: AssistantMessageProps) {
+  const [responseText, setResponseText] = useState('');
+
+  useEffect(() => {
+    console.log('conversationId', conversationId);
+
+    // HACK: ConversationId will be empty when the message is first created
+    if (!conversationId) return;
+
+    const eventSource = new EventSource(
+      `${import.meta.env.VITE_API_URL}/conversation/${conversationId}/stream`
+    );
+
+    eventSource.onmessage = (event) => {
+      const { data } = event;
+      setResponseText((prev) => prev + data);
+    };
+
+    eventSource.onerror = () => {
+      eventSource.close();
+    };
+
+    eventSource.addEventListener('end', () => {
+      eventSource.close();
+    });
+
+    return () => {
+      eventSource.close();
+    };
+  }, [conversationId]);
+
+  return <div>{!responseText ? <LoadingDots /> : <>{responseText}</>}</div>;
 }
