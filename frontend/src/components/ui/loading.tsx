3b12b9ee6b2ed1d418dc947d17b82f039988b0da
bryanguillen <bryan.guillen217@gmail.com>
2025-02-28 09:20:51 -0500

    Initial commit

---
+++ frontend/src/components/ui/loading.tsx
@@ -0,0 +1,9 @@
+export function LoadingDots() {
+  return (
+    <div className="flex items-center justify-center space-x-1">
+      <div className="h-2 w-2 animate-bounce rounded-full bg-black"></div>
+      <div className="h-2 w-2 animate-bounce rounded-full bg-black delay-200"></div>
+      <div className="h-2 w-2 animate-bounce rounded-full bg-black delay-400"></div>
+    </div>
+  );
+}
---
+++ frontend/src/pages/home/components/AssistantMessage.tsx
@@ -0,0 +1,5 @@
+import { LoadingDots } from '@/components/ui/loading';
+
+export function AssistantMessage() {
+  return <LoadingDots />;
+}
