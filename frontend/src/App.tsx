6b6b4c2b7ae2e97c6a97add75ced66c6164ba5b5
bryanguillen <bryan.guillen217@gmail.com>
2025-03-03 14:13:49 -0500

    Add Toaster

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
-import { AppInitializer } from '@/components';
+import { AppInitializer, Toaster } from '@/components';
 import { Home } from '@/pages/home';
 
 const App = () => {
   return (
     <AppInitializer>
       <Home />
+      <Toaster />
     </AppInitializer>
   );
 };
