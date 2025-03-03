2d560d0906b0a424d69daa4be55f70b1839dfe16
bryanguillen <bryan.guillen217@gmail.com>
2025-03-03 09:35:38 -0500

    Refactor usage of message container

--- frontend/src/pages/home/components/Messages.tsx
+++ frontend/src/pages/home/components/Messages.tsx
@@ -2,6 +2,8 @@ import { useEffect, useRef } from 'react';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { MessageDto, SenderType } from '../types';
 import { AssistantMessage } from './AssistantMessage';
+import MessageContainer from './MessageContainer';
+
 interface MessagesProps {
   messages: MessageDto[];
 }
@@ -16,39 +18,21 @@ export default function Messages({ messages }: MessagesProps) {
   return (
     <ScrollArea className="h-full flex-1 overflow-y-auto p-4">
       <div className="space-y-4">
-        {messages.map(({ id, ...message }) => (
-          <MessageContainer key={id} {...message}>
-            {message.senderType === SenderType.ASSISTANT ? (
-              <AssistantMessage
-                conversationId={message.conversationId}
-                messagesEndRef={messagesEndRef}
-              />
-            ) : (
-              message.content
-            )}
-          </MessageContainer>
-        ))}
+        {messages.map(({ id, senderType, ...message }) =>
+          senderType === SenderType.ASSISTANT ? (
+            <AssistantMessage
+              key={id}
+              conversationId={message.conversationId}
+              messagesEndRef={messagesEndRef}
+            />
+          ) : (
+            <MessageContainer key={id} senderType={SenderType.USER}>
+              {message.content}
+            </MessageContainer>
+          )
+        )}
         <div ref={messagesEndRef} />
       </div>
     </ScrollArea>
-  );
-}
-
-interface MessageContainerProps {
-  children: string | React.ReactNode;
-  senderType: SenderType;
-}
-
-function MessageContainer({ senderType, children }: MessageContainerProps) {
-  return (
-    <div
-      className={`flex ${senderType === SenderType.USER ? 'justify-end' : 'justify-start'}`}
-    >
-      <div
-        className={`max-w-[90%] rounded-lg px-4 py-2 ${senderType === SenderType.USER ? 'bg-primary text-primary-foreground' : 'bg-muted'}`}
-      >
-        <div className="mb-1 whitespace-pre-wrap">{children}</div>
-      </div>
-    </div>
   );
 }
