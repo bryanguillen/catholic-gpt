b306da99a2208a08356f6bd63d7b5a022e0eb0ba
bryanguillen <bryan.guillen217@gmail.com>
2025-03-03 09:51:06 -0500

    Add loading spinner

--- frontend/src/components/ui/loading.tsx
+++ frontend/src/components/ui/loading.tsx
@@ -8,4 +8,10 @@ function LoadingDots() {
   );
 }
 
-export { LoadingDots };
+function LoadingSpinner() {
+  return (
+    <div className="h-10 w-10 animate-spin rounded-full border-t-4 border-b-4 border-gray-900 dark:border-white"></div>
+  );
+}
+
+export { LoadingDots, LoadingSpinner };
