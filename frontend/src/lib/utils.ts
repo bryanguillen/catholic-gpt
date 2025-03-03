5c7bc4254ccac54bae2cf176a023eda28ac90ad9
bryanguillen <bryan.guillen217@gmail.com>
2025-03-03 14:31:10 -0500

    Add support for logging console.error in dev

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function logErrorInDev(...args: any[]) {
+  if (import.meta.env.MODE === 'development') {
+    console.log(...args);
+  }
+}
