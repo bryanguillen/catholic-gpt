852cfc86368911296418d01b18cb3eb52e78af2c
bryanguillen <bryan.guillen217@gmail.com>
2025-03-04 05:03:29 -0500

    Import and use utility fn
    Used to have the util fn declared in the module.

--- frontend/src/pages/home/hooks/useConversationMessages/useConversationMessages.ts
+++ frontend/src/pages/home/hooks/useConversationMessages/useConversationMessages.ts
@@ -12,6 +12,7 @@ import {
   SaveUserMessageResponseDto,
 } from '@/types';
 import { logErrorInDev } from '@/lib/utils';
+import { optimisticConversationMessageReducer } from './optimisticConversationMessageReducer';
 
 interface UseConversationMessagesResults {
   conversationId: string | null;
@@ -26,27 +27,7 @@ export const useConversationMessages = (): UseConversationMessagesResults => {
   const [optimisticMessages, setOptimisticMessages] = useOptimistic<
     MessageDto[],
     MessageDto[]
-  >(messages, (messages, optimisticMessages) => {
-    const realIds = new Set(messages.map((item) => item.id));
-
-    /**
-     * If duplicate found, means that the optimistic message has been sent and received.
-     * Thus, the optimistic message is no longer needed.
-     *
-     * There is a moment where both the optimistic and real messages will exists, and thus, if we simply
-     * merge the optimistic messages, we will have duplicate messages in the UI,
-     * which creates problems with the keys for the messages.
-     *
-     * To avoid this, we return the real messages only if a duplicate is found.
-     */
-    const duplicateFound = optimisticMessages.some((item) =>
-      realIds.has(item.id)
-    );
-
-    return duplicateFound
-      ? [...messages]
-      : [...messages, ...optimisticMessages];
-  });
+  >(messages, optimisticConversationMessageReducer);
 
   const [appUserId] = useLocalStorageState('userId');
 
