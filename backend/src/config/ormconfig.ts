a285091d9a905e718ff53decbbe15295f0858ed0
bryanguillen <bryan.guillen217@gmail.com>
2025-03-04 14:32:13 -0500

    Update to use url for the db

--- backend/src/config/ormconfig.ts
+++ backend/src/config/ormconfig.ts
@@ -5,11 +5,7 @@ config();
 
 const dataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || '5432', 10),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  url: process.env.DATABASE_URL,
   entities: ['dist/**/*.entity.js'],
   migrations: ['dist/migrations/*.js'],
   synchronize: false,
